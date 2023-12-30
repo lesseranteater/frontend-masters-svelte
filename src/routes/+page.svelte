719c5ced72279fb5ce4328e20e3ce624f877d012
Alex <alex.iconia.tab@gmail.com>
2023-12-30 22:31:03 +0200

    Introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-Hello World
+<script>
+  import Nested from "./Nested.svelte";
+  let name = "Alex";
+  let src = "/favicon.png";
+  let string = "this string contains some <strong>HTML!!!</strong>";
+</script>
+
+<h1>Hello, {name.toUpperCase()}</h1>
+<img {src} alt="Svelte favicon" />
+<p>This is a paragraph</p>
+<Nested />
+{@html string}
+
+<style>
+  p {
+    color: goldenrod;
+    font-family: "Comic Sans MS", cursive;
+    font-size: 2em;
+  }
+</style>
